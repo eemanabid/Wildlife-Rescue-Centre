ca6938ea1b28ec1e13e61efbf613ecfdcf59a5f4
Hareem Khan <hareem.khan@ucalgary.ca>
2023-03-20 14:46:56 -0600

    getTaskIDTest

--- edu/ucalgary/oop/WildlifeRescueTests.java
+++ edu/ucalgary/oop/WildlifeRescueTests.java
@@ -1,10 +1,31 @@
 package edu.ucalgary.oop;
 
 import org.junit.Test;
+import org.junit.validator.PublicClassValidator;
+
 import static org.junit.Assert.*;
 import org.junit.*;
 
 public class WildlifeRescueTests {
+
+    public WildlifeRescueTests() {}
+
+    @Test
+    public void getTaskIDTest() {
+        Task newTask = new Task(1, 20, "Rebandage fox leg wound", 40);
+
+        int taskGetID = newTask.getTaskID;
+        int expectedResult = 1;
+        assertEquals(expectedResult, taskGetID);
+        
+    }
+
+    public void setTaskIDTest() {
+        
+        
+    }
+
+
 
 
 
