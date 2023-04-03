b50b6335584bdf738e7a91bb0304dd7640c85ede
Hooriya Amjad <hooriyaamjad@Hooriyas-MacBook-Pro.local>
2023-04-03 15:30:48 -0600

    added volunteer constructor

--- edu/ucalgary/oop/Volunteer.java
+++ edu/ucalgary/oop/Volunteer.java
@@ -5,4 +5,13 @@ public class Volunteer {
     private String firstName;
     private String lastName;
     private String phoneNumber;
+
+    public Volunteer(String firstName, String lastName, String volunteerID, String phoneNumber){
+        this.firstName = firstName; 
+        this.lastName = lastName;
+        this.volunteerID = volunteerID;
+        this.phoneNumber = phoneNumber;
+    }
+
+    
 }
