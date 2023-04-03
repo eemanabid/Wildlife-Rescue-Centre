2ed8682599efebc237bf501cbf185ce21d9c9243
Hooriya Amjad <hooriyaamjad@Hooriyas-MacBook-Pro.local>
2023-04-03 15:23:20 -0600

    added volunteer class

---
+++ edu/ucalgary/oop/Volunteer.java
@@ -0,0 +1,8 @@
+package edu.ucalgary.oop;
+
+public class Volunteer {
+    private String volunteerID;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+}
