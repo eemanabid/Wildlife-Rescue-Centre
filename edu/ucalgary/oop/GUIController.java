f4b83457c097eb73588aef7e725fa9ef4ff963e9
Hooriya Amjad <hooriyaamjad@Hooriyas-MacBook-Pro.local>
2023-04-04 01:09:48 -0600

    added basic begin, about and quit buttons on a main menu for GUI innterface

--- edu/ucalgary/oop/GUIController.java
+++ edu/ucalgary/oop/GUIController.java
@@ -9,7 +9,43 @@ import java.awt.event.ActionListener;
 public class GUIController {
     private final JFrame FRM = new JFrame();
 
+    //empty constructor atm: 
     public GUIController(){
-        
+    }
+
+    public void mainMenu(){
+        JPanel mainMenu = new JPanel();
+
+        JButton start = new JButton( new AbstractAction("Begin") {
+            @Override
+            public void actionPerformed( ActionEvent e ) {
+                mainMenu.setVisible(false);
+                //menuForCategory();
+            }
+        });
+
+        JButton about = new JButton( new AbstractAction("About") {
+            @Override
+            public void actionPerformed( ActionEvent e ) {
+                mainMenu.setVisible(false);
+                //about();
+            }
+        });
+
+        JButton exit = new JButton( new AbstractAction("Quit") {
+            @Override
+            public void actionPerformed( ActionEvent e ){
+                System.exit(0);
+            }
+        });
+
+        mainMenu.add(start);
+        mainMenu.add(about);
+        mainMenu.add(exit);
+        FRM.add(mainMenu);
+        FRM.setTitle("WildLife Rescue Centre");
+        FRM.setVisible(true);
+        FRM.setSize(500, 100);
+        FRM.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
