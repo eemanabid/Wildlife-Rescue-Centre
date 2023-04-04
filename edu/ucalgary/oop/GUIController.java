cdf7a550492188b214296af3ad7c31539a8d91bc
Hooriya Amjad <hooriyaamjad@Hooriyas-MacBook-Pro.local>
2023-04-04 17:46:00 -0600

    added comments/plan on what to do for gui?

--- edu/ucalgary/oop/GUIController.java
+++ edu/ucalgary/oop/GUIController.java
@@ -7,6 +7,25 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GUIController {
+    /*
+     * When the user clicks the "Generate Schedule" button, 
+     * the program will attempt to generate a schedule based on the animal and medical task data
+     * If the schedule cannot be generated, an error message will be displayed in the status bar.
+     * needs to be able to modify the start hour of one or more of the treatments 
+     * based on the error received?
+     * 
+     * When the user clicks the "Save Schedule" button, 
+     * the program will save the current schedule to a file on disk. 
+     * If an error occurs while saving the file, 
+     * an error message will be displayed in the status bar.
+     * 
+     * When the user clicks the "Get Confirmation" button, 
+     * the program will confirm that a backup volunteer is needed. 
+     * If the backup volunteer cannot be reached, 
+     * an error message will be displayed in the status bar.
+     */
+
+
     private final JFrame FRM = new JFrame();
 
     //empty constructor atm: 
@@ -20,8 +39,7 @@ public class GUIController {
             @Override
             public void actionPerformed( ActionEvent e ) {
                 mainMenu.setVisible(false);
-                // menuForCategory();
-                // add function which generates the EWR schedule?
+                
             }
         });
 
