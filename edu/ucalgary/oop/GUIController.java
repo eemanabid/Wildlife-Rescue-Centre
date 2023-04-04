a327afcf97bf5b196183a8b9c5bce46613923788
Hooriya Amjad <hooriyaamjad@Hooriyas-MacBook-Pro.local>
2023-04-04 01:01:00 -0600

    started GUI class

---
+++ edu/ucalgary/oop/GUIController.java
@@ -0,0 +1,15 @@
+package edu.ucalgary.oop;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUIController {
+    private final JFrame FRM = new JFrame();
+
+    public GUIController(){
+        
+    }
+}
