74afa40dbccb3114ecdcf650e08c4ca0026c615e
Hareem Khan <hareem.khan@ucalgary.ca>
2023-04-09 23:50:45 -0600

    commit

--- edu/ucalgary/oop/RescueCenter.java
+++ edu/ucalgary/oop/RescueCenter.java
@@ -21,8 +21,8 @@ public class RescueCenter {
     private Connection dbConnect;
     private ResultSet results;
     private final String DBURL = "jdbc:mysql://localhost/EWR";
-    private final String USERNAME = "root";
-    private final String PASSWORD = "_Hakh@8905";
+    private final String USERNAME = "oop";
+    private final String PASSWORD = "password";
 
     // constructor
     public RescueCenter(){
