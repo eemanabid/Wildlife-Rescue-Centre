319995d0857d6fa360264129159bbe88690f6fbb
eemanabid <abideeman@gmail.com>
2023-04-10 23:49:16 -0600

    commit

--- edu/ucalgary/oop/GUIController.java
+++ edu/ucalgary/oop/GUIController.java
@@ -498,10 +498,15 @@ public class GUIController implements ScheduleFormatter{
                     int newStartHour = Integer.parseInt(JOptionPane.showInputDialog(FRM, "Enter new start hour for Treatment " + treatmentID + ":", currentStartHour));
                     if (newStartHour > 23 || newStartHour < 0){
                         JOptionPane.showMessageDialog(FRM, "New start hour is invalid, please input an hour from 0-23", "Error", JOptionPane.ERROR_MESSAGE);
-                        newStartHour = currentStartHour; 
+                        if (newStartHour > currentStartHour + maxWindow - 1) { // check if the new start hour is outside the max window
+                            JOptionPane.showMessageDialog(FRM, "New start hour is outside the maximum window for Treatment " + treatmentID + " (max window is " + maxWindow + " hours).", "Error", JOptionPane.ERROR_MESSAGE);
+                        }  
                     }
-                    if (newStartHour > currentStartHour + maxWindow - 1) { // check if the new start hour is outside the max window
+                    else if (newStartHour > currentStartHour + maxWindow - 1) { // check if the new start hour is outside the max window
                         JOptionPane.showMessageDialog(FRM, "New start hour is outside the maximum window for Treatment " + treatmentID + " (max window is " + maxWindow + " hours).", "Error", JOptionPane.ERROR_MESSAGE);
+                        if (newStartHour > 23 || newStartHour < 0){
+                            JOptionPane.showMessageDialog(FRM, "New start hour is invalid, please input an hour from 0-23", "Error", JOptionPane.ERROR_MESSAGE);
+                        }
                     } 
                     else {
                         try {
